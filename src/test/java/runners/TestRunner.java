42f707c1edad615179269c59f67c6cc5b6904d90
aiR2729 <riapuspita.27@gmail.com>
2025-07-13 22:02:34 +0700

    Run test and upload report

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -2,13 +2,17 @@ package runners;
 
 import org.junit.platform.suite.api.*;
 
-import static io.cucumber.junit.platform.engine.Constants.*;
-
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "stepdefinitions")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+@ConfigurationParameter(
+        key = io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME,
+        value = "stepdefinitions"
+)
+@ConfigurationParameter(
+        key = io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME,
+        value = "pretty"
+)
 public class TestRunner {
 }
 
